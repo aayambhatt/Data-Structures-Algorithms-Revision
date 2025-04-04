67f0ea4a8b872d6844d2d776e14312356935264b
aayambhatt <aayambhatt23@gmail.com>
2025-04-04 23:02:16 +0530

    majority-element-1

---
+++ 01-Array-Problems/Majority-Element-1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+        int majorityElement(vector<int>& nums) {
+            int count = 0;
+            int el;
+            int n = nums.size();
+    
+            for(int i = 0; i<n ; i++){
+                if(count==0){
+                    el = nums[i];
+                    count =1;
+                }
+    
+                else if(nums[i]==el){
+                    count++;
+                }
+                else{
+                    count--;;
+                }
+    
+            }
+            return el;
+            
+        }
+    };
+
+int main()
+{
+ return 0;
+}
