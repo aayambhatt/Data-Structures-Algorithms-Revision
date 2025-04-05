d45ebd9638e116c86f064e88fc5732e13cdf2071
aayambhatt <aayambhatt23@gmail.com>
2025-04-05 15:53:28 +0530

    reverse string 2

---
+++ 05-Strings/Reverse-string-II.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+        string reverseStr(string s, int k) {
+            int n = s.length();
+    
+            for(int i = 0; i<n ; i+=2*k){
+                int left = i;
+                int right = min(i+k-1,n-1);
+    
+                while(left<right){
+                    swap(s[left],s[right]);
+                    left++;
+                    right--;
+                }
+            }
+            
+            return s;
+        }
+    };
+
+int main()
+{
+ return 0;
+}
