22c45765fb892c07d727650fb528b739a4bd6fdf
aayambhatt <aayambhatt23@gmail.com>
2025-04-05 15:53:54 +0530

    string

---
+++ 05-Strings/String.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    // char arr[] ={'a', 'p', 'p', 'l', 'e'};
+    // for(int i = 0; i<5; i++ ){
+    //     cout<<arr[i];
+    // }
+
+    // //variable lenght
+    // char arr[20];
+    // cin>>arr;
+    // arr[2]='\0';
+    // cout<<arr<<endl;
+
+     //string
+    // string S = "Aayam";
+    // cout<<S;
+
+    // string s1;
+    // cin>>s1;
+    // cout<<s1;
+
+    string s3;
+    getline(cin, s3);
+    cout<<s3;
+
+
+ return 0;
+}
