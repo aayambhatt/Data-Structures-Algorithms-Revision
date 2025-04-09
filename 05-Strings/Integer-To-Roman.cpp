9361c25ed6faab618e334a3f7db6bc15c052b569
aayambhatt <aayambhatt23@gmail.com>
2025-04-09 22:29:24 +0530

    integer to roman

---
+++ 05-Strings/Integer-To-Roman.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+        string intToRoman(int num) {
+            vector<int> values =     {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            vector<string> symbols = {"M",  "CM","D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+            string result = "";
+
+            for(int i =0 ; i<values.size() ; i++){
+                while(num>=values[i]){
+                    result = result + symbols[i];
+                    num = num - values[i];
+                }
+            }
+            
+            return result;
+            
+        }
+    };
+
+int main()
+{
+ return 0;
+}
